db81e7e332361edde6e30bcd5a90f8d386a53740
418shambhavit-droid <418shambhavit@gmail.com>
2026-04-06 17:56:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+const icon = themeToggle.querySelector('i');
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+    
+    // Toggle the icon
+    if (body.classList.contains('dark-theme')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+});
